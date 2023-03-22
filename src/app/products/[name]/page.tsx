97ee4b01ce8896c435c0093d7f5f87ca4274bc05
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-22 14:05:40 +0100

    lastest attempt to deploy

--- src/app/products/[name]/page.tsx
+++ src/app/products/[name]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 const getData = async (name: string) => {
-  const data = await fetch(`nextjs-testing-7tih5ykwz-smooreswe.vercel.app/api/hello?name=${name}`,
+  const data = await fetch(`https://nextjs-testing-ii3thn6ut-smooreswe.vercel.app/api/hello?name=${name}`,
   {cache: 'no-store'});
   const response = await data.json()
   return response;
